1e7d5258b3eb0d7a9f1f26646f959ea35983c4ab
newbluestudios <newbluestudios@gmail.com>
2020-07-27 15:31:26 +0800

    Name and Phone Number Form and serverless api endpoint to connect to twilio

--- pages/api/opt-in.js
+++ pages/api/opt-in.js
@@ -1,3 +1,13 @@
+import _ from 'lodash'
+
 export default (req, res) => {
-  res.status(200).json({ text: 'Hello' });
+  if (req.method === 'POST') {
+    console.log('post')
+  } else {
+    console.log('not post')
+  }
+  
+  const {name, phone} = _.get(req, 'body')
+  console.log('🤖 -Server Side - ', req.body)
+  res.status(200).json({name, phone});
 };
--- pages/opt-in.js
+++ pages/opt-in.js
@@ -1,10 +1,55 @@
-
+import React, {useState, useCallback, useEffect } from 'react'
+import Router from 'next/router'
+import _ from 'lodash'
 
 export default () => {
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
 
-  return(
-    <div>
-      Let's put a form here
-    </div>
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    
+    fetch('/api/opt-in', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name,
+        phone,
+      }),
+    }).then((res) => {
+      // 💫 Do a fast client-side transition to the prefetched page
+      if (res.ok) Router.push('/')
+    })
+  }
+
+  useEffect(() => {
+    // Prefetch the next page
+    Router.prefetch('/')
+  }, [])
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type='text'
+          value={name}
+          onChange={(event)=>setName(event.target.value)}
+          placeholder='Name'
+        />
+      </label>
+      
+      <label>
+        Phone Number:
+        <input
+          type='text'
+          value={phone}
+          onChange={(event)=>setPhone(event.target.value)}
+          placeholder='Phone Number'
+        />
+      </label>
+
+      <button type="submit">Opt In</button>
+    </form>
   )
 }
